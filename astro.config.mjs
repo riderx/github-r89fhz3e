b29a818a1700d74afc9c90cb3813b78f7630ca71
Martin Donadieu <martindonadieu@gmail.com>
2025-12-04 23:48:25 +0000

    base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,17 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import { filterSitemapByDefaultLocale, i18n } from 'astro-i18n-aut/integration';
 import sitemap from '@astrojs/sitemap';
+import starlightLlmsTxt from 'starlight-llms-txt'
 const defaultLocale = 'en';
 const localeNames = {
   en: 'en-US', // the `defaultLocale` value must present in `locales` keys
   es: 'es-ES',
 };
+const locales = Object.keys(localeNames);
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://example.com/',
   i18n: {
     locales,
     defaultLocale,
@@ -26,13 +29,17 @@ export default defineConfig({
     }),
     sitemap({
       i18n: {
-        locales,
         defaultLocale,
+        locales: localeNames,
       },
       filter: filterSitemapByDefaultLocale({ defaultLocale }),
     }),
     starlight({
       title: 'My Docs',
+      pagefind: false,
+      prerender: true,
+      disable404Route: true,
+      plugins: [starlightLlmsTxt()],
       social: [
         {
           icon: 'github',
@@ -45,7 +52,7 @@ export default defineConfig({
           label: 'Guides',
           items: [
             // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', slug: 'guides/example' },
+            { label: 'Example Guide', autogenerate: { directory: 'guides' } },
           ],
         },
         {
